6f8a02c0411c98befb944f16567e97e542c90412
xuliang <514132096@qq.com>
2020-02-05 18:45:38 +0800

    删除无用的文件，新增客户、结算单控制器

---
+++ src/main/java/com/dili/settlement/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.dili.settlement.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 客户相关controller
+ */
+@Controller
+@RequestMapping(value = "/customer")
+public class CustomerController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
+
+
+}
---
+++ src/main/java/com/dili/settlement/controller/SettleOrderController.java
@@ -0,0 +1,18 @@
+package com.dili.settlement.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 由MyBatis Generator工具自动生成
+ * This file was generated on 2020-02-05 16:39:11.
+ */
+
+@Controller
+@RequestMapping("/settleOrder")
+public class SettleOrderController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SettleOrderController.class);
+
+}
