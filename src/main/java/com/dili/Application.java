73678599ca53119150a3c40edff58d91940242c5
wangmi <87730066>
2020-02-18 11:08:35 +0800

    更新配置，解决启动报错

--- src/main/java/com/dili/Application.java
+++ src/main/java/com/dili/Application.java
@@ -19,7 +19,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @MapperScan(basePackages = {"com.dili.settlement.mapper", "com.dili.ss.dao"})
 @ComponentScan(basePackages={"com.dili.ss","com.dili.settlement","com.dili.uap.sdk"})
-@RestfulScan({"com.dili.settlement.rpc","com.dili.uap.sdk.rpc"})
+@RestfulScan({"com.dili.uap.sdk.rpc"})
 @DTOScan(value={"com.dili.ss", "com.dili.settlement.domain"})
 @EnableDiscoveryClient
 @EnableFeignClients
